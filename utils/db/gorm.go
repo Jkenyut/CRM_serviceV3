44c7f009962fe1d9407b698f4bd3b137d7718f20
Jkenyut <satrianursaputro06@gmail.com>
2023-06-09 14:00:58 +0700

    feat : feat to looping connect db and to migrate

--- utils/db/gorm.go
+++ utils/db/gorm.go
@@ -6,15 +6,34 @@ import (
 	"gorm.io/gorm/logger"
 	"log"
 	"os"
+	"time"
 )
 
 func GormMysql() *gorm.DB {
-	dsn := os.Getenv("CONNECT_DB") + os.Getenv("DB_HOST") + os.Getenv("CONNECT_DB_LAST")
-	var db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
-	db = db.Debug()
-	if err != nil {
-		log.Println("gorm.open", err)
-	}
-	return db
+	var db *gorm.DB
+	var err error
 
+	for {
+		// Attempt to establish a database connection
+		db, err = connectToMySQL()
+		if err == nil {
+			break // Connection successful, break out of the loop
+		}
+
+		log.Println("Failed to connect to the database:", err)
+		time.Sleep(3 * time.Second) // Wait for 3 seconds before the next attempt
+	}
+
+	return db
+}
+
+func connectToMySQL() (*gorm.DB, error) {
+	dsn := os.Getenv("CONNECT_DB") + os.Getenv("DB_HOST") + os.Getenv("CONNECT_DB_LAST")
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	if err != nil {
+		return nil, err
+	}
+
+	db = db.Debug()
+	return db, nil
 }
